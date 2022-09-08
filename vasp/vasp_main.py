44450d7eb17edb933afcb0dd6c1e00a001b8cefe
mayuan_physics_mearchine <myth620137018@163.com>
2022-09-08 09:27:02 +0800

    fix little carelessness about submitopt-fine timeout problem

--- vasp/vasp_main.py
+++ vasp/vasp_main.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 '''
-vasp_main.py -i ./test/POSCAR          -w ./test/ -j pbs relax -m encut=400 kspacing=0.3 mode=rv3
-vasp_main.py -i ./test/POSCAR          -w ./test/ -j pbs relax -m encut=400 kspacing=0.3 mode=rvf
-vasp_main.py -i ./test/POSCAR          -w ./test/ -j pbs phono -m supercell=[2,2,2] kpoints=[40,40,40] mode=disp   
-vasp_main.py -i ./test/POSCAR          -w ./test/ -j pbs phono -m supercell=[2,2,2] kpoints=[40,40,40] mode=dfpt
-vasp_main.py -i ./test/0.0/POSCAR-init -w ./test/        phono -m mode=dispprog supercell=[2,2,2]
-vasp_main.py -i ./test/0.0/POSCAR-init -w ./test/        phono -m mode=dfptprog supercell=[2,2,2]
-vasp_main.py -i ./test                 -w ./opt   -j pbs batchrelax -m encut=400 kspacing=0.3 mode=rv3
-vasp_main.py -i ./test                 -w ./opt   -j pbs batchrelax -m encut=400 kspacing=0.3 mode=rvf
+vasp_main.py -i ./test/POSCAR          -w ./test/ -j pbs -p 200 relax      -m  mode=rv3
+vasp_main.py -i ./test/POSCAR          -w ./test/ -j pbs -p 200 relax      -m  encut=400 kspacing=0.3 mode=rvf
+vasp_main.py -i ./test/POSCAR          -w ./test/ -j pbs -p 200 phono      -m  supercell=[2,2,2] kpoints=[40,40,40] mode=disp   
+vasp_main.py -i ./test/POSCAR          -w ./test/ -j pbs -p 200 phono      -m  supercell=[2,2,2] kpoints=[40,40,40] mode=dfpt
+vasp_main.py -i ./test/0.0/POSCAR-init -w ./test/        -p 200 phono      -m  mode=dispprog supercell=[2,2,2]
+vasp_main.py -i ./test/0.0/POSCAR-init -w ./test/        -p 200 phono      -m  mode=dfptprog supercell=[2,2,2]
+vasp_main.py -i ./test                 -w ./opt   -j pbs -p 200 batchrelax -m  encut=400 kspacing=0.3 mode=rv3
+vasp_main.py -i ./test                 -w ./opt   -j pbs -p 200 batchrelax -m  encut=400 kspacing=0.3 mode=rvf
 
 
 vasp_main.py -i ./test/POSCAR -w ./test/ -j slurm relax -m encut=400 kspacing=0.3 mode=rv3 queue=lhy
