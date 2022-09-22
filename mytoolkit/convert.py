006c3770c32cba5573771ad36858961ff9b22299
mayuan <mayuan20@mails.jlu.edu.cn>
2022-09-22 16:11:13 +0800

    let convert tool to output primitive cell all the time

--- mytoolkit/convert.py
+++ mytoolkit/convert.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 from ase.io import read
 from ase.io.wien2k import write_struct
+from ase.spacegroup import crystal
 
 def convert(
     input_file_path,
@@ -15,14 +16,15 @@ def convert(
     input_file_path = Path(input_file_path)
     work_path       = Path(work_path)
     struct          = read(input_file_path)
+    struct_primitive= crystal(symbols=struct, primitive_cell=True)
     formula         = struct.get_chemical_formula("metal")
 
     if dst_format == "cif":
         dst_file = work_path.joinpath(formula+".cif")
-        struct.write(dst_file, format="cif")
+        struct_primitive.write(dst_file, format="cif")
     elif dst_format == "vasp":
         dst_file = work_path.joinpath(formula+".vasp")
-        struct.write(dst_file, format="vasp")
+        struct_primitive.write(dst_file, format="vasp")
     elif dst_format == "wien2k":
         dst_file = work_path.joinpath(formula+".struct")
-        write_struct(dst_file, struct)
+        write_struct(dst_file, struct_primitive)
