0d432858e778bdf3c7c3503310f0ca6da6b30ffa
mayuan <mayuan20@mails.jlu.edu.cn>
2022-09-28 16:23:11 +0800

    update .gitignore

--- vasp/vaspbin.py
+++ vasp/vaspbin.py
@@ -1,14 +1,14 @@
-vaspbin_path = "/work/home/may/software/vasp.6.1.0/bin/vasp_std"
-intel_compiler = "/work/home/may/intel/oneapi/setvars.sh"
-potcar_source_libs = "/work/home/may/POT/vasp_pot1/potpaw_PBE54"
+#vaspbin_path = "/work/home/may/software/vasp.6.1.0/bin/vasp_std"
+#intel_compiler = "/work/home/may/intel/oneapi/setvars.sh"
+#potcar_source_libs = "/work/home/may/POT/vasp_pot1/potpaw_PBE54"
 
 # vaspbin_path = "/public/home/mayuan/software/vasp.6.1.0/bin/vasp_std"
 # intel_compiler = "/public/home/mayuan/intel/oneapi/setvars.sh"
 # potcar_source_libs = "/public/home/mayuan/POT/vasp_pot1/potpaw_PBE54"
 
-# vaspbin_path = "/work/software/vasp.6.1.0-oneapi2022.2.0/bin/vasp_std"
-# intel_compiler = "/work/env/oneapi-2022.2.0 "
-# potcar_source_libs = "/work/home/mayuan/POT/vasp_pot1/potpaw_PBE54"
+vaspbin_path = "/work/software/vasp.6.1.0-oneapi2022.2.0/bin/vasp_std"
+intel_compiler = "/work/env/oneapi-2022.2.0 "
+potcar_source_libs = "/work/home/mayuan/POT/vasp_pot1/potpaw_PBE54"
 
 
 
