35f8b94d05aafe61287340e01c02f66ad0f073c9
mayuan_physics_mearchine <myth620137018@163.com>
2023-02-20 10:24:07 +0800

    增加了一个写mcsqs的输入文件的脚本

--- sqs-inputfile-scripts/slurm/2produce_sqscell.sh
+++ sqs-inputfile-scripts/slurm/2produce_sqscell.sh
@@ -8,6 +8,6 @@
  wait
 
 
-# 在运行完上面的命令后，会得到一个叫sqscell.out的文件。
+# 在运行完上面的命令后，会得到一个叫sqscell.out的文件.
 # 当然也会产生bestcorr.out文件和bestsqs.out文件，但是我们主要目的还是获得sqscell.out文件。
 # 不太清楚这个文件是干什么用的。但是一旦它出现以后，就将任务停掉。
