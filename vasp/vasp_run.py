fee71953e49771e9e4abd423e859bb886dbc0c7a
mayuan <mayuan20@mails.jlu.edu.cn>
2023-03-31 14:54:05 +0800

    完成了vasp的data的数据处理

--- vasp/vasp_run.py
+++ vasp/vasp_run.py
@@ -238,48 +238,45 @@ class vasp_processdata(vasp_base):
     def __init__(self, args: ArgumentParser) -> None:
 
         # read input para
-        self._config = config(args).read_config()
+        self._config         = config(args).read_config()
         self.input_file_path = Path(self._config['input_file_path'])
         if self._config['work_path']:
-            self.work_path = Path(self._config['work_path'])
+            self.work_path   = Path(self._config['work_path'])
         else:
-            self.work_path = Path.cwd()
+            self.work_path   = Path.cwd()
         self.mode            = self._config['mode']
-        # the default value of spectrum and dos is False, 
-        # that means the program will not create the phono-spectrum, total-dos, pdos
-        self.spectrum        = eval(self._config.get('spectrum', "False"))
-        self.dos             = eval(self._config.get('dos', "False"))
-
-        self.ase_type          = read(self.input_file_path)
-        self.struct_type       = AseAtomsAdaptor.get_structure(self.ase_type)
+        self.ase_type        = read(self.input_file_path)
+        self.struct_type     = AseAtomsAdaptor.get_structure(self.ase_type)
         self.get_struct_info(self.struct_type, self.work_path)
         
+        if self.mode == "dispband" or self.mode == "dfptband":
+            self.post_progress_phono_band()
+        if self.mode == "dispphdos" or self.mode == "dispphdos":
+            self.post_progress_phono_dos()
+        if self.mode == "eband":
+            self.post_progress_eletron_band()
+        if self.mode == "eledos":
+            self.post_progress_eletron_dos()
+    
+    # 绘制 phonoband 
+    def post_progress_phono_band(self):
+
         if "supercell" in self._config:
             _supercell = self._config['supercell'].split()
             self.supercell = list(map(int, _supercell))
         else:
-            raise ValueError("you have to specify the supercell=[?,?,?]")
+            print("WARNING: ----------------------------- ")
+            raise ValueError("you have to specify the supercell=[?,?,?]. If you didn't specify it, maybe somthing wrong will occur !")
 
         if "mp" in self._config:
             _mp = self._config['mp'].split()
             self.mp = list(map(int, _mp))
         else:
             self.mp = [8,8,8]
+            print("NOTES: ------------------------------ ")
             print("you didn't specify the mp='? ? ?', the program will set default mp=[8,8,8]")
 
-        if "pdos" in self._config:
-            self.pdos = self._config['pdos']
-        else:
-            self.pdos = "AUTO"
-
-        if self.spectrum:
-            self.post_progress_phono_spectrum()
-        if self.dos:
-            self.post_progress_phono_dos()
-
-    def post_progress_phono_spectrum(self):
-
-        if self.mode == "dispprog":
+        if self.mode == "dispband":
 
             _disp_num = len(list(Path(self.work_path).glob("disp-*")))
             disp_num = str(_disp_num).rjust(3, '0')
@@ -304,7 +301,7 @@ class vasp_processdata(vasp_base):
             os.system("phonopy-bandplot  --gnuplot> band.dat")
             os.chdir(cwd)
 
-        elif self.mode == "dfptprog":
+        elif self.mode == "dfptband":
 
             cwd = os.getcwd()
             os.chdir(self.work_path)
@@ -325,52 +322,14 @@ class vasp_processdata(vasp_base):
             os.system("phonopy --dim='{}' -p -s band.conf -c POSCAR-init".format(' '.join(list(map(str, self.supercell)))))
             os.system("phonopy-bandplot  --gnuplot> band.dat")
             os.chdir(cwd)
-        
-    def write_disp_band_conf(
-        self, 
-        band_conf_dirpath, 
-        species, 
-        supercell, 
-        path_name_list, 
-        path_coords
-        ):
-        __species = [spe.name for spe in species]
-        __supercell = list(map(str, supercell))
-
-        band_conf_filepath = os.path.join(band_conf_dirpath, "band.conf")
-        with open(band_conf_filepath, "w") as f:
-            f.write("ATOM_NAME={}            \n".format(' '.join(__species)))
-            f.write("DIM={}                  \n".format(' '.join(__supercell)))
-            f.write("NPOINTS=101             \n")
-            f.write("EIGENVECTORS=.TRUE.     \n")
-            f.write("BAND_LABELS={}          \n".format(' '.join(path_name_list)))
-            path_coords = list(chain.from_iterable(path_coords)); path_coords=list(map(str, path_coords))
-            f.write("BAND={}                 \n".format(' '.join(path_coords)))  
-
-    def write_dfpt_band_conf(
-        self, 
-        band_conf_dirpath, 
-        species, 
-        supercell,  
-        special_points, 
-        path_coords
-        ):
-        
-        __species = [spe.name for spe in species]
-        __supercell = list(map(str, supercell))
-        
-        band_conf_filepath = os.path.join(band_conf_dirpath, "band.conf")
-        with open(band_conf_filepath, "w") as f:
-            f.write("FORCE_CONSTANTS=READ    \n")
-            f.write("ATOM_NAME={}            \n".format(' '.join(__species)))
-            f.write("DIM={}                  \n".format(' '.join(__supercell)))
-            f.write("NPOINTS=101             \n")
-            f.write("EIGENVECTORS=.TRUE.     \n")
-            f.write("BAND_LABELS={}          \n".format(' '.join(special_points)))
-            path_coords = list(chain.from_iterable(path_coords)); path_coords=list(map(str, path_coords))
-            f.write("BAND={}                 \n".format(' '.join(path_coords)))
-
+    
+    # 绘制 phonodos 
     def post_progress_phono_dos(self):
+
+        if "phdos" in self._config:
+            self.pdos = self._config['pdos']
+        else:
+            self.pdos = "AUTO"
 
         if self.mode == "dispprog": 
             # 获得total_dos.dat
@@ -387,7 +346,7 @@ class vasp_processdata(vasp_base):
             os.chdir(cwd)
 
             # 获得pdos.dat
-            self.write_disp_pdos_conf(
+            self.write_disp_phdos_conf(
                 self.work_path, 
                 self.species, 
                 self.supercell, 
@@ -414,7 +373,7 @@ class vasp_processdata(vasp_base):
             os.chdir(cwd)
 
             # 获得pdos.dat
-            self.write_dfpt_pdos_conf(
+            self.write_dfpt_phdos_conf(
                 self.work_path, 
                 self.species, 
                 self.supercell, 
@@ -426,82 +385,8 @@ class vasp_processdata(vasp_base):
             os.system("phonopy -p pdos.conf -c {}".format(self.input_file_path.name)) # 获得 total_dos.dat
             os.chdir(cwd)
     
-    # 创建mesh.conf  目的为了获得 total dos 
-    def write_disp_mesh_conf(
-        self,
-        mesh_conf_dirpath, 
-        species, 
-        supercell,
-        mp,
-        ): 
-
-        __species = [spe.name for spe in species]
-        __supercell = list(map(str, supercell))
-        __mp = list(map(str, mp))
-        mesh_conf_filepath = os.path.join(mesh_conf_dirpath, "mesh.conf")
-        with open(mesh_conf_filepath, "w") as f:
-            f.write("ATOM_NAME={}            \n".format(' '.join(__species)))
-            f.write("DIM={}                  \n".format(' '.join(__supercell)))
-            f.write("MP ={}                  \n".format(' '.join(__mp)))
-    # 创建mesh.conf  目的为了获得 total dos  
-    def write_dfpt_mesh_conf(
-        self,
-        mesh_conf_dirpath, 
-        species, 
-        supercell,
-        mp,
-        ): 
-
-        __species = [spe.name for spe in species]
-        __supercell = list(map(str, supercell))
-        __mp = list(map(str, mp))
-        mesh_conf_filepath = os.path.join(mesh_conf_dirpath, "mesh.conf")
-        with open(mesh_conf_filepath, "w") as f:
-            f.write("ATOM_NAME={}            \n".format(' '.join(__species)))
-            f.write("DIM={}                  \n".format(' '.join(__supercell)))
-            f.write("MP ={}                  \n".format(' '.join(__mp)))
-            f.write("FORCE_CONSTANTS = READ  \n")
-    # 创建pdos.conf  目的为了获得 pdos 
-    def write_disp_pdos_conf(
-        self,
-        pdos_conf_dirpath, 
-        species, 
-        supercell,
-        mp,
-        pdos,
-        ): 
-
-        __species = [spe.name for spe in species]
-        __supercell = list(map(str, supercell))
-        __mp = list(map(str, mp))
-        pdos_conf_filepath = os.path.join(pdos_conf_dirpath, "pdos.conf")
-        with open(pdos_conf_filepath, "w") as f:
-            f.write("ATOM_NAME={}            \n".format(' '.join(__species)))
-            f.write("DIM={}                  \n".format(' '.join(__supercell)))
-            f.write("MP ={}                  \n".format(' '.join(__mp)))
-            f.write("PDOS = {}               \n".format(pdos))
-    # 创建pdos.conf  目的为了获得 pdos 
-    def write_dfpt_pdos_conf(
-        self,
-        pdos_conf_dirpath, 
-        species, 
-        supercell,
-        mp,
-        pdos,
-        ): 
-
-        __species = [spe.name for spe in species]
-        __supercell = list(map(str, supercell))
-        __mp = list(map(str, mp))
-        pdos_conf_filepath = os.path.join(pdos_conf_dirpath, "pdos.conf")
-        with open(pdos_conf_filepath, "w") as f:
-            f.write("ATOM_NAME={}            \n".format(' '.join(__species)))
-            f.write("DIM={}                  \n".format(' '.join(__supercell)))
-            f.write("MP ={}                  \n".format(' '.join(__mp)))
-            f.write("FORCE_CONSTANTS = READ  \n")
-            f.write("PDOS = {}               \n".format(pdos))
-
-    def post_progress_eband(self):
+    # 绘制 eband
+    def post_progress_eletron_band(self):
 
         import matplotlib.pyplot as plt
         from pymatgen.io.vasp.outputs import Vasprun
@@ -520,8 +405,9 @@ class vasp_processdata(vasp_base):
         eband_fig.get_plot(bs=eband)
         ebandpng_path = self.sub_workpath.joinpath('eband.png')
         plt.savefig(ebandpng_path, img_format='png')
-
-    def post_progress_edos(self):
+    
+    # 绘制 eledos
+    def post_progress_eletron_dos(self):
 
         import matplotlib.pyplot as plt
         from pymatgen.io.vasp.outputs import Vasprun
@@ -534,12 +420,139 @@ class vasp_processdata(vasp_base):
 
         e_fermi_fromvasp = vasprun.efermi
         e_fermi_frompmg  = vasprun.calculate_efermi(float=0.0001)
-
+        print("NOTES: ------------------------------ ")
+        print("    You have to confirm that the Fermi energy is from scf/OUTCAR. Because the Fermi energy in dos/DOSCAR is not accurate")
+        print("    You cat use 'grep E-fermi scf/OUTCAR' to check the Fermi energy! ")
         # set figure parameters, draw figure
         eledos_fig = BSDOSPlotter(bs_projection=None, dos_projection=None, vb_energy_range=5, fixed_cb_energy=5)
         eledos_fig.get_plot(bs=eledos)
         eledospng_path = self.sub_workpath.joinpath('eband.png')
         plt.savefig(eledospng_path, img_format='png')
+    
+    # 创建band.conf  目的为了获得 band of phonon
+    def write_disp_band_conf(
+        self, 
+        band_conf_dirpath, 
+        species, 
+        supercell, 
+        path_name_list, 
+        path_coords
+        ):
+        __species = [spe.name for spe in species]
+        __supercell = list(map(str, supercell))
+
+        band_conf_filepath = os.path.join(band_conf_dirpath, "band.conf")
+        with open(band_conf_filepath, "w") as f:
+            f.write("ATOM_NAME={}            \n".format(' '.join(__species)))
+            f.write("DIM={}                  \n".format(' '.join(__supercell)))
+            f.write("NPOINTS=101             \n")
+            f.write("EIGENVECTORS=.TRUE.     \n")
+            f.write("BAND_LABELS={}          \n".format(' '.join(path_name_list)))
+            path_coords = list(chain.from_iterable(path_coords)); path_coords=list(map(str, path_coords))
+            f.write("BAND={}                 \n".format(' '.join(path_coords)))  
+    
+    # 创建band.conf  目的为了获得 band of phonon
+    def write_dfpt_band_conf(
+        self, 
+        band_conf_dirpath, 
+        species, 
+        supercell,  
+        special_points, 
+        path_coords
+        ):
+        
+        __species = [spe.name for spe in species]
+        __supercell = list(map(str, supercell))
+        
+        band_conf_filepath = os.path.join(band_conf_dirpath, "band.conf")
+        with open(band_conf_filepath, "w") as f:
+            f.write("FORCE_CONSTANTS=READ    \n")
+            f.write("ATOM_NAME={}            \n".format(' '.join(__species)))
+            f.write("DIM={}                  \n".format(' '.join(__supercell)))
+            f.write("NPOINTS=101             \n")
+            f.write("EIGENVECTORS=.TRUE.     \n")
+            f.write("BAND_LABELS={}          \n".format(' '.join(special_points)))
+            path_coords = list(chain.from_iterable(path_coords)); path_coords=list(map(str, path_coords))
+            f.write("BAND={}                 \n".format(' '.join(path_coords)))
+    
+    # 创建mesh.conf  目的为了获得 total dos 
+    def write_disp_mesh_conf(
+        self,
+        mesh_conf_dirpath, 
+        species, 
+        supercell,
+        mp,
+        ): 
+
+        __species = [spe.name for spe in species]
+        __supercell = list(map(str, supercell))
+        __mp = list(map(str, mp))
+        mesh_conf_filepath = os.path.join(mesh_conf_dirpath, "mesh.conf")
+        with open(mesh_conf_filepath, "w") as f:
+            f.write("ATOM_NAME={}            \n".format(' '.join(__species)))
+            f.write("DIM={}                  \n".format(' '.join(__supercell)))
+            f.write("MP ={}                  \n".format(' '.join(__mp)))
+    
+    # 创建mesh.conf  目的为了获得 total dos  
+    def write_dfpt_mesh_conf(
+        self,
+        mesh_conf_dirpath, 
+        species, 
+        supercell,
+        mp,
+        ): 
+
+        __species = [spe.name for spe in species]
+        __supercell = list(map(str, supercell))
+        __mp = list(map(str, mp))
+        mesh_conf_filepath = os.path.join(mesh_conf_dirpath, "mesh.conf")
+        with open(mesh_conf_filepath, "w") as f:
+            f.write("ATOM_NAME={}            \n".format(' '.join(__species)))
+            f.write("DIM={}                  \n".format(' '.join(__supercell)))
+            f.write("MP ={}                  \n".format(' '.join(__mp)))
+            f.write("FORCE_CONSTANTS = READ  \n")
+    
+    # 创建pdos.conf  目的为了获得 pdos 
+    def write_disp_phdos_conf(
+        self,
+        pdos_conf_dirpath, 
+        species, 
+        supercell,
+        mp,
+        pdos,
+        ): 
+
+        __species = [spe.name for spe in species]
+        __supercell = list(map(str, supercell))
+        __mp = list(map(str, mp))
+        pdos_conf_filepath = os.path.join(pdos_conf_dirpath, "pdos.conf")
+        with open(pdos_conf_filepath, "w") as f:
+            f.write("ATOM_NAME={}            \n".format(' '.join(__species)))
+            f.write("DIM={}                  \n".format(' '.join(__supercell)))
+            f.write("MP ={}                  \n".format(' '.join(__mp)))
+            f.write("PDOS = {}               \n".format(pdos))
+    
+    # 创建pdos.conf  目的为了获得 pdos 
+    def write_dfpt_phdos_conf(
+        self,
+        pdos_conf_dirpath, 
+        species, 
+        supercell,
+        mp,
+        pdos,
+        ): 
+
+        __species = [spe.name for spe in species]
+        __supercell = list(map(str, supercell))
+        __mp = list(map(str, mp))
+        pdos_conf_filepath = os.path.join(pdos_conf_dirpath, "pdos.conf")
+        with open(pdos_conf_filepath, "w") as f:
+            f.write("ATOM_NAME={}            \n".format(' '.join(__species)))
+            f.write("DIM={}                  \n".format(' '.join(__supercell)))
+            f.write("MP ={}                  \n".format(' '.join(__mp)))
+            f.write("FORCE_CONSTANTS = READ  \n")
+            f.write("PDOS = {}               \n".format(pdos))
+
 
 class vasp_clear:
 
