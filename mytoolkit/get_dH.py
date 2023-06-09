8e95226532d8e89ea68ad3e41ff603067b13838e
mayuan_physics_mearchine <myth620137018@163.com>
2023-06-09 16:26:19 +0800

    完成了get_dH*的修改

--- mytoolkit/get_dH.py
+++ mytoolkit/get_dH.py
@@ -9,6 +9,7 @@ try:
 except:
     print("   OUTCAR有问题读不出来焓值")
     dH = 100000000000000.0
+    N =1
 
 begin_id = os.popen('grep -n "position of ions in cartesian coordinates" OUTCAR').read().split(":")[0]
 N = 0; row_id=int(begin_id)
