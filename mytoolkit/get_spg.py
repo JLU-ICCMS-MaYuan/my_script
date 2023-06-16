79370b3a1be26db0b515ef41ee117c2858a8b034
mayuan_physics_mearchine <myth620137018@163.com>
2023-06-16 11:14:12 +0800

    对get_spg.py做了小小的修改

--- mytoolkit/get_spg.py
+++ mytoolkit/get_spg.py
@@ -5,10 +5,19 @@ import sys
 from pymatgen.core.structure import Structure
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 
-filename = sys.argv[1]
+try:
+    print("Please input filename and symprec, for example:")
+    print("    get_spg.py CONTCAR 1e-5")
+    filename = sys.argv[1]
+    symprec  = float(sys.argv[2])
+except:
+    print("You input nothing, so the default values will be used")
+    print("    filename is POSCAR, symprec is 1e-3")
+    filename = "POSCAR"
+    symprec  = 1e-3
 
 struct = Structure.from_file(filename)
-spgaly = SpacegroupAnalyzer(struct, symprec = 0.001,)
+spgaly = SpacegroupAnalyzer(struct, symprec = 0.001)
 symbol = spgaly.get_space_group_symbol()
 spgnum = spgaly.get_space_group_number()
 
