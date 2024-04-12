ee9bc3e7cfa50b5e1bfffcb70d2e1a4e0d864443
mayuan <mayuan20@mails.jlu.edu.cn>
2024-04-12 10:10:32 +0800

    修改了投影轨道求和的脚本

--- mytoolkit/plus_dosorbit_for_vasp.py
+++ mytoolkit/plus_dosorbit_for_vasp.py
@@ -5,25 +5,9 @@ import pandas as pd
 
 from ase.io import read
 
-atoms = read('POSCAR')
-vol = atoms.get_volume()
-print('vol=', vol)
 
-eles = sys.argv[1:]
-
-df = pd.read_table("PDOS_H.dat", sep='\s+')
-H_tdos = df['tot']
-energy = df['#Energy']
-
-df = pd.read_table("PDOS_Be.dat", sep='\s+')
-Be_tdos = df['tot']
-
-t_d_orb = 0
-t_f_orb = 0
-t_s_orb = 0
-t_p_orb = 0
-# 遍历重元素
-for e in eles:
+def plus_spdf(e):
+    t_s_orb, t_p_orb, t_d_orb, t_f_orb, tot = [0,0,0,0,0]
     df = pd.read_table(f"PDOS_{e}.dat", sep='\s+')
     s_orb = df['s'] 
     p_orb = df['py'] + df['pz'] + df['px']
@@ -33,24 +17,45 @@ for e in eles:
     t_p_orb += p_orb
     t_d_orb += d_orb
     t_f_orb += f_orb
+    tot = df['tot']
+    return t_s_orb, t_p_orb, t_d_orb, t_f_orb, tot
 
-# 创建新的DataFrame
-new_df = pd.DataFrame({
-    '#Energy': energy,
-    '_s,'.join(eles)+'_s,'+'_p,'.join(eles)+'_p': (t_s_orb + t_p_orb)/vol,
-    '_d,'.join(eles)+'_d': t_d_orb/vol,
-    '_f,'.join(eles)+'_f': t_f_orb/vol,
-    'Be': Be_tdos/vol,
-    'H': H_tdos/vol,
-    'TDOS': (H_tdos + Be_tdos + t_f_orb + t_d_orb + t_s_orb + t_p_orb)/vol,
-})
-nearest_to_zero_index = new_df['#Energy'].abs().idxmin()
+atoms = read('POSCAR')
+vol = atoms.get_volume()
+print('vol=', vol)
+
+eles = sys.argv[1:]
+
+# 选择一个元素来获取 'Energy' 列
+sample_element = eles[0]
+sample_df = pd.read_table(f"PDOS_{sample_element}.dat", sep='\s+')
+energy = sample_df['#Energy']
+# 创建一个空 DataFrame 用于存储结果，并添加 'Energy' 列
+result_df = pd.DataFrame({'#Energy': energy})
+
+
+# 遍历元素
+for e in eles:
+    df = pd.read_table(f"PDOS_{e}.dat", sep='\s+')
+    s_orb, p_orb, d_orb, f_orb, tot = plus_spdf(e)
+    
+    # 将每个元素的 spdf 轨道和 tot 轨道添加到结果 DataFrame 中
+    result_df[f'{e}_s'] = s_orb
+    result_df[f'{e}_p'] = p_orb
+    result_df[f'{e}_d'] = d_orb
+    result_df[f'{e}_f'] = f_orb
+    result_df[f'{e}_tot'] = tot
+
+# 将结果写入文件
+result_df.to_csv('spdf_tot_result.csv', index=False)
+
+nearest_to_zero_index = result_df['#Energy'].abs().idxmin()
 # 获取最接近0的行的上下5行索引范围
 start_index = max(0, nearest_to_zero_index - 3)
-end_index = min(len(new_df), nearest_to_zero_index + 3)
+end_index = min(len(result_df), nearest_to_zero_index + 3)
 
 # 输出选取的行
 print("上下5行数据:")
-print(new_df.iloc[start_index:end_index])
+print(result_df.iloc[start_index:end_index])
 
-new_df.to_csv('final.csv',index=False)
+result_df.to_csv('final.csv',index=False)
