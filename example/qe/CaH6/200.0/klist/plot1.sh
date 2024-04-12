5c80cde30d7be173eb531f827a9e2c8fc0cb8629
mayuan <mayuan20@mails.jlu.edu.cn>
2024-04-12 10:47:20 +0000

    删除文件 example/qe/CaH6/200.0

--- example/qe/CaH6/200.0/klist/plot1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source /work/env/intel2020
-ifort -o dir2cart.x dir2cart.f90
-ifort -o kpath_20.x kpath_20.f
-
-./dir2cart.x > kpath.in
-mv kpath.in kpath.ini
-./kpath_20.x  
--- example/qe/CaH6/200.0/s8_phonodos.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh   
-source /public/home/mayuan/intel/oneapi/setvars.sh --force
-ulimit -s unlimited
-mpirun -np 1 /public/home/mayuan/software/qe-7.1/bin/matdyn.x  <phonodos.in> phonodos.out  
