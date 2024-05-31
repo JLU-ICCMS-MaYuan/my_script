1600792dcf25a19f8ab726eb947677c065fd0a78
mayuan <mayuan20@mails.jlu.edu.cn>
2024-05-31 14:58:49 +0800

    整理了mytoolkit里面的文件

---
+++ write_script_path.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+scripts_names = [
+    'ares-phonon-toolkit', 
+    'bader-toolkit',
+    'calypso-toolkit', 
+    'lobster-toolkit', 
+
+    'mytoolkit/convexhull', 
+    'mytoolkit/energy_curve_with_pressure', 
+    'mytoolkit/files_tidy',
+    'mytoolkit/job_tasks_submit',
+    'mytoolkit/pieces',
+    'mytoolkit/qe_toolkit',
+    'mytoolkit/structures_characters',
+    'mytoolkit/superconductivity',
+    'mytoolkit/vasp_toolkit',
+    'mytoolkit/wien2K',
+
+    'uspex-toolkit', 
+    ]
+
+
+with open('~/.myenvs', 'w') as f:
+    for dirname in scripts_names:
+        abs_path = Path(dirname).absolute()
+        f.write(f'export PATH="{abs_path}:$PATH"\n')
+        
