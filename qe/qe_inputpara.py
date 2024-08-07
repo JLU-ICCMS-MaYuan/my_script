a875e56f254637161b21f4329359ba3121a9d5ac
mayuan <mayuan20@mails.jlu.edu.cn>
2024-08-07 16:45:41 +0800

    修复qe_inputpara.py中check_convergence的bug

--- qe/qe_inputpara.py
+++ qe/qe_inputpara.py
@@ -498,11 +498,12 @@ class qephono_inputpara(qe_inputpara):
         # 检查 ph_no_split.in 文件是否存在，如果存在就，获取里面的展宽间距el_ph_sigma和展宽个数el_ph_nsigma
         ph_no_split_in_path = self.work_path.joinpath("ph_no_split.in")
         if ph_no_split_in_path.exists():
-            greporder = f"grep el_ph_sigma {ph_no_split_in_path}"
-            el_ph_sigma = float(re.search(r'\d+\.\d+', os.popen(greporder).read()).group())
-            greporder = f"grep el_ph_nsigma {ph_no_split_in_path}"
-            el_ph_nsigma = int(re.search(r'\d+', os.popen(greporder).read()).group())
-            gaussian0 = [el_ph_sigma*(1+i) for i in range(0, el_ph_nsigma)]
+            # greporder = f"grep el_ph_sigma {ph_no_split_in_path}"
+            # el_ph_sigma = float(re.search(r'\d+\.\d+', os.popen(greporder).read()).group())
+            # greporder = f"grep el_ph_nsigma {ph_no_split_in_path}"
+            # el_ph_nsigma = int(re.search(r'\d+', os.popen(greporder).read()).group())
+            el_ph_sigma = self.el_ph_sigma
+            gaussian0 = [el_ph_sigma*(1+i) for i in range(0, self.el_ph_nsigma)]
         else:
             print("\nNote: --------------------")
             print("     The program didn't get `ph_no_split.in`. So you need input it by yourself.")
