4915841060e0b8a74c03959e8fc1803ed14953e4
mayuan <mayuan20@mails.jlu.edu.cn>
2024-09-15 12:55:30 +0800

    修改了get_dH_peratoms.py和get_dE_peratoms.py

--- mytoolkit/vasp_toolkit/get_dE_peratoms.py
+++ mytoolkit/vasp_toolkit/get_dE_peratoms.py
@@ -3,27 +3,46 @@
 import os
 import sys
 import re
+import subprocess
 
-try:
-    dH = os.popen('grep "free  energy   TOTEN" OUTCAR' + " | tail -n 1 | awk '{print $ 5}'").read().strip('\n')
-    if not dH:
-        dH = 100000000000000.0
-except:
-    print("   OUTCAR有问题读不出来焓值")
-    dH = 100000000000000.0
+def get_free_energy(outcar_path):
+    try:
+        result = subprocess.run("grep -s " + " free  energy   TOTEN " + f" {outcar_path} | tail -n 1 | awk '{{print $5}}'", 
+                                shell=True, capture_output=True, text=True)
+        dE = result.stdout.strip()
+        return float(dE) if dE else 1e14
+    except Exception:
+        return 1e14
 
-try:
-    begin_id = os.popen('grep -n "position of ions in cartesian coordinates" OUTCAR').read().split(":")[0]
-    N = 0; row_id=int(begin_id)
-    while True:
-        row_id = row_id+1
-        content  = os.popen("sed -n '{}p' OUTCAR".format(row_id)).read().strip('\n')
-        corrds   = re.findall(r"[-+]?\d+\.\d+", content)
-        if len(corrds) == 3:
-            N += 1
-        else:
-            break
-except:
-    N = 1
+def get_num_atoms(outcar_path):
+    try:
+        result = subprocess.run(f'grep -n -s "position of ions in cartesian coordinates" {outcar_path}', 
+                                shell=True, capture_output=True, text=True)
+        begin_id = int(result.stdout.split(":")[0])
 
-print("{:<12.8f} {:<3}".format(float(dH)/N, N))
+        N = 0
+        with open(outcar_path, 'r') as f:
+            for i, line in enumerate(f, start=1):
+                if i > begin_id:
+                    coords = re.findall(r"[-+]?\d+\.\d+", line.strip())
+                    if len(coords) == 3:
+                        N += 1
+                    else:
+                        break
+        return N
+    except Exception:
+        return 1
+
+def main():
+    try:
+        outcar_path = sys.argv[1]
+    except IndexError:
+        outcar_path = "OUTCAR"
+    
+    dE = get_free_energy(outcar_path)
+    N = get_num_atoms(outcar_path)
+    
+    print("{:<12.8f} {:<3}".format(dE/N, N))
+
+if __name__ == "__main__":
+    main()
