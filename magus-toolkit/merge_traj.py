866e3dc99eb8351df2bda641fd1ab69a6a369b44
mayuan <mayuan20@mails.jlu.edu.cn>
2024-12-20 16:26:12 +0800

    修改了merge_traj.py的命令行读取方式

--- magus-toolkit/merge_traj.py
+++ magus-toolkit/merge_traj.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
-
-#!/usr/bin/env python3
 import os
-import sys
+import argparse
 from ase.io import read, write
 
-print("Please input TRAJ files in the order you wish to merge")
-filenames = sys.argv[1:]
+# 设置命令行参数解析器
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Merge multiple TRAJ files into a single TRAJ file.")
+    parser.add_argument('-i', '--input_files', nargs='+', help="List of input TRAJ files to merge")
+    parser.add_argument('-o', '--output', required=True, help="Output file name (e.g., merged.traj)")
+    return parser.parse_args()
 
-atoms_list = []
-for filename in filenames:
-    # 读取每个traj文件的所有帧
-    traj_frames = read(filename, index=':')
-    print('filename:{}\nframes found={}\nframes read={}'.format(filename, len(traj_frames), len(traj_frames)))
-    atoms_list.extend(traj_frames)
+def main():
+    # 解析命令行参数
+    args = parse_arguments()
+    input_files = args.input_files
+    output_file = args.output
 
-print('Total frames: {}'.format(len(atoms_list)))
+    atoms_list = []
+    for filename in input_files:
+        # 读取每个traj文件的所有帧
+        traj_frames = read(filename, index=':')
+        print('filename:{}\nframes found={}\nframes read={}'.format(filename, len(traj_frames), len(traj_frames)))
+        atoms_list.extend(traj_frames)
 
-# 将所有结构写入一个新的traj文件
-write("merged.traj", atoms_list)
+    print('Total frames: {}'.format(len(atoms_list)))
+
+    # 将所有结构写入一个新的traj文件
+    write(output_file, atoms_list)
+    print(f"All frames have been saved to {output_file}")
+
+if __name__ == '__main__':
+    main()
