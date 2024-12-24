02133d03ccfeceb4d402f217fd474ded22afa82b
mayuan <mayuan20@mails.jlu.edu.cn>
2024-12-24 20:17:46 +0800

    修改了write_lammpsdata.py

--- lammps-toolkit/write_lammpsdata.py
+++ lammps-toolkit/write_lammpsdata.py
@@ -3,9 +3,7 @@ import sys
 import argparse
 
 from ase.io import read
-from ase.build.tools import sort
 from ase.io.lammpsdata import write_lammps_data
-from ase.io.lammpsrun import read_lammps_dump_text
 
 parser = argparse.ArgumentParser(description='Generate supercells with optional diagonal or non-diagonal supercell matrices.')
 # 输入文件
@@ -15,12 +13,24 @@ parser.add_argument('-i', '--input', type=str, default='POSCAR',
 # 元素排序顺序
 parser.add_argument('-o', '--order', nargs='+', type=str,
                     help='Custom element order for sorting, e.g., O C H for ordering oxygen first, followed by carbon and hydrogen, You have to make sure the order you specified is consistant with the order of input files')
+
 args = parser.parse_args()
 
 if "POSCAR" in args.input or ".vasp" in args.input:
-    atoms = read(args.input)
+    new_frame = read(args.input)
+    write_lammps_data('data.lammps', new_frame, specorder=args.order)
 elif ".lammpstrj" in args.input:
-    atoms = read_lammps_dump_text(open(args.input), index=-1)
+    # type_to_symbol = {i: j for i, j in enumerate(args.symbols)}  # i: [0,1,2], j: [Ce, Sr, H]
+    # symbol_to_type = {v: k for k, v in type_to_symbol.items()}   # 用于输出cfg时的映射 v: [Ce, Sr, H], k: [0,1,2]
+    atoms = read(open(args.input), format='lammps-dump-text', index=':')
+    print(f"读取到 {len(atoms)} 帧轨迹")
     print("get last frame of trajectory")
-    
-write_lammps_data('data.lammps', atoms, specorder=args.order)
+    new_frame = atoms[-1]
+
+# 按照指定的元素顺序排序
+element_to_index = {element: idx for idx, element in enumerate(args.order)}
+# 获取排序后的索引
+sorted_indices = sorted(range(len(new_frame)), key=lambda i: element_to_index[new_frame.get_chemical_symbols()[i]])
+# 根据指定顺序排列原子
+new_frame = new_frame[sorted_indices]
+write_lammps_data('data.lammps', new_frame)
