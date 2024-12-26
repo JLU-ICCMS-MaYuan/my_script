a1ef907599d6e5b9382bba98f9e12a84ccaef9eb
mayuan <mayuan20@mails.jlu.edu.cn>
2024-12-26 16:08:53 +0800

    修改了make_traj_file.py

--- magus-toolkit/make_traj_file.py
+++ magus-toolkit/make_traj_file.py
@@ -14,14 +14,14 @@ def parse_arguments():
 
         )
     )
-    parser.add_argument('-i', '--input-dir',          type=str, required=True, help='Directory or pattern containing the structure files.')
+    parser.add_argument('-i', '--input-dirs',          type=str, nargs='+', required=True, help='Directory or pattern containing the structure files.')
     parser.add_argument('-c', '--detailed-conditions',type=str, nargs='+', required=True, help='List of subdirectories or patterns for detailed selection')
     parser.add_argument('-o', '--output-file',        type=str, required=True, help='Output traj file name')
-    parser.add_argument('-t', '--file-types',          type=str, nargs='+', default=['POSCAR*'], help='List of type of file types, default=[POSCAR*]')
+    parser.add_argument('-t', '--file-types',          type=str, nargs='+', default=['POSCAR*'], help='List of type of file types, default=[POSCAR*], more situations is *vasp ')
     return parser.parse_args()
 
 
-def search_files(input_dir, file_types, detailed_conditions):
+def search_files(input_dirs, file_types, detailed_conditions):
     # 搜索所有匹配的文件
 
     if os.path.exists("dst_files.dat"):
@@ -30,14 +30,15 @@ def search_files(input_dir, file_types, detailed_conditions):
     f = open("dst_files.dat", 'a')
     files = []
     for file_type in file_types:
-        # print(glob.glob(os.path.join(input_dir, '**', file_type), recursive=True))
-        for dsttype_file in glob.glob(os.path.join(input_dir, '**', file_type), recursive=True):
-            for condition in detailed_conditions:
-                if condition not in dsttype_file:
-                    break
-            else:
-                files.append(dsttype_file)
-                f.write(os.path.abspath(dsttype_file)+'\n')
+        # print(glob.glob(os.path.join(input_dirs, '**', file_type), recursive=True))
+        for input_dir in input_dirs:
+            for dsttype_file in glob.glob(os.path.join(input_dir, '**', file_type), recursive=True):
+                for condition in detailed_conditions:
+                    if condition not in dsttype_file:
+                        break
+                else:
+                    files.append(dsttype_file)
+                    f.write(os.path.abspath(dsttype_file)+'\n')
     f.close()
 
     return files
@@ -47,7 +48,7 @@ def search_files(input_dir, file_types, detailed_conditions):
 def main():
     # 解析命令行参数
     args = parse_arguments()
-    input_dir = args.input_dir
+    input_dirs = args.input_dirs
     output_file = args.output_file
     detailed_conditions = args.detailed_conditions
     file_types = args.file_types
@@ -55,7 +56,7 @@ def main():
     # 使用glob模块来匹配文件类型，支持递归查找
     # 遍历多个用户指定的子目录或模式
 
-    dst_files = search_files(input_dir, file_types, detailed_conditions)
+    dst_files = search_files(input_dirs, file_types, detailed_conditions)
     
     # 创建一个列表来存储所有的结构
     atoms_list = []
