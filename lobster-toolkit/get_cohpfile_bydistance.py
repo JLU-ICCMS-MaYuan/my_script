0b9ca388c0b689af806602240aa753a262b30878
mayuan <mayuan20@mails.jlu.edu.cn>
2025-01-10 16:12:10 +0800

    修改了get_cohpfile_bydistance.py

--- lobster-toolkit/get_cohpfile_bydistance.py
+++ lobster-toolkit/get_cohpfile_bydistance.py
@@ -1,79 +1,88 @@
 #!/usr/bin/env python
 import os
 import sys
-from pprint import pprint
-from collections import defaultdict
-from itertools import combinations, product
-
-import numpy as np
+import argparse
 from pymatgen.core.structure import Structure
 
-info="""使用说明:
-python get_pairs.py [COHPstartEnergy] [COHPendEnergy] [element1] [element2] [lower_d_limit] [upper_d_limit]
-指定其实计算的能量COHPstartEnergy和终止计算的能量COHPendEnergy
-指定两种元素element1和element2, 两种元素可以相同, 效果是求两种元素在单胞内所有原子间距离
-指定原子间距离，获得指定距离下原子对。
-"""
-print(info)
 
-COHPstartEnergy = sys.argv[1]
-COHPendEnergy   = sys.argv[2]
-species_custom1 = sys.argv[3]
-species_custom2 = sys.argv[4]
-
-try:
-    lower_d = float(sys.argv[5])
-    upper_d = float(sys.argv[6])
-except:
-    lower_d = 0.0
-    upper_d = 5.0
+# 设置命令行参数解析器
+parser = argparse.ArgumentParser(description="生成LOBSTER输入文件")
+parser.add_argument('-e', '--COHPEnergy', nargs="+", type=float, help="COHP起始终止能量")
+parser.add_argument('-s', '--species_custom', nargs="+", type=str, help="元素1, 2")
+parser.add_argument('-d', '--d_limit', nargs="+", type=float, help="原子间最小最大距离")
+parser.add_argument('-m', '--mode', choices=['5', '0'], required=True, help="选择生成的输入文件版本：'5' 或 '0'")
+args = parser.parse_args()
+# 提取参数
+COHPstartEnergy = args.COHPEnergy[0]
+COHPendEnergy = args.COHPEnergy[1]
+species_custom1 = args.species_custom[0]
+species_custom2 = args.species_custom[1]
+lower_d = args.d_limit[0]
+upper_d = args.d_limit[1]
+mode = args.mode  # 选择生成的输入文件版本
 
 
 struct = Structure.from_file("POSCAR")
 
-with open("lobsterinIsmear_5", "w") as f:
-    f.write('COHPstartEnergy  {}\n'.format(COHPstartEnergy)) 
-    f.write('COHPendEnergy    {}\n'.format(COHPendEnergy))
-    f.write('usebasisset pbeVaspFit2015\n') # 基组（直接使用根据vasp拟合的基组以及默认的基函数）
-    for spe in struct.types_of_specie:
-        f.write('basisfunctions {}\n'.format(spe.name)) # 基组（直接使用根据vasp拟合的基组以及默认的基函数）
-    # 这里会出现一个非常严重的计算问题！！！！！！！！！！！！！！
-    # lobster认为你指定的原子对是不具有周期性的
-    # 你用pymatgen脚本找到的距离是包含周期性的，把这原子对输入给lobsterin
-    # 它认不出来这个距离是周期性的，它会按照原胞内的距离考虑两个原子的成键。
-    # 所以这里我抛弃了设置原子对来计算成键强度的方法。
-    # 改用设置键长来获得原子对，lobster有自己的算法来获得原子对。
-    # for pair, idx, d in pairs_idxs_d:
-    #     if lower_d <= d <= upper_d:
-    #         f.write("cohpbetween atom {} and atom {}\n".format(idx[0], idx[1]))
-    f.write("cohpGenerator from {} to {} type {} type {}\n".format(lower_d, upper_d, species_custom1, species_custom2))
-
-with open("lobsterinIsmear_0", "w") as f:
-    f.write('COHPstartEnergy  {}\n'.format(COHPstartEnergy)) 
-    f.write('COHPendEnergy    {}\n'.format(COHPendEnergy))
-    f.write('usebasisset pbeVaspFit2015\n') # 基组（直接使用根据vasp拟合的基组以及默认的基函数）
-    f.write('gaussianSmearingWidth 0.05\n')
-    for spe in struct.types_of_specie:
-        f.write('basisfunctions {}\n'.format(spe.name)) # 基组（直接使用根据vasp拟合的基组以及默认的基函数）
-    # 这里会出现一个非常严重的计算问题！！！！！！！！！！！！！！
-    # lobster认为你指定的原子对是不具有周期性的
-    # 你用pymatgen脚本找到的距离是包含周期性的，把这原子对输入给lobsterin
-    # 它认不出来这个距离是周期性的，它会按照原胞内的距离考虑两个原子的成键。
-    # 所以这里我抛弃了设置原子对来计算成键强度的方法。
-    # 改用设置键长来获得原子对，lobster有自己的算法来获得原子对。
-    # for pair, idx, d in pairs_idxs_d:
-    #     if lower_d <= d <= upper_d:
-    #         f.write("cohpbetween atom {} and atom {}\n".format(idx[0], idx[1]))
-    f.write("cohpGenerator from {} to {} type {} type {}\n".format(lower_d, upper_d, species_custom1, species_custom2))
+def write_lobsterin(dirname, mode=5):
+    if mode == 5:
+        with open(dirname+"/lobsterin", "w") as f:
+            f.write('COHPstartEnergy  {}\n'.format(COHPstartEnergy)) 
+            f.write('COHPendEnergy    {}\n'.format(COHPendEnergy))
+            f.write('usebasisset pbeVaspFit2015\n') # 基组（直接使用根据vasp拟合的基组以及默认的基函数）
+            for spe in struct.types_of_specie:
+                f.write('basisfunctions {}\n'.format(spe.name)) # 基组（直接使用根据vasp拟合的基组以及默认的基函数）
+            # 这里会出现一个非常严重的计算问题！！！！！！！！！！！！！！
+            # lobster认为你指定的原子对是不具有周期性的
+            # 你用pymatgen脚本找到的距离是包含周期性的，把这原子对输入给lobsterin
+            # 它认不出来这个距离是周期性的，它会按照原胞内的距离考虑两个原子的成键。
+            # 所以这里我抛弃了设置原子对来计算成键强度的方法。
+            # 改用设置键长来获得原子对，lobster有自己的算法来获得原子对。
+            # for pair, idx, d in pairs_idxs_d:
+            #     if lower_d <= d <= upper_d:
+            #         f.write("cohpbetween atom {} and atom {}\n".format(idx[0], idx[1]))
+            f.write("cohpGenerator from {} to {} type {} type {}\n".format(lower_d, upper_d, species_custom1, species_custom2))
+    elif mode == 0:
+        with open(dirname+"/lobsterin", "w") as f:
+            f.write('COHPstartEnergy  {}\n'.format(COHPstartEnergy)) 
+            f.write('COHPendEnergy    {}\n'.format(COHPendEnergy))
+            f.write('usebasisset pbeVaspFit2015\n') # 基组（直接使用根据vasp拟合的基组以及默认的基函数）
+            f.write('gaussianSmearingWidth 0.05\n')
+            for spe in struct.types_of_specie:
+                f.write('basisfunctions {}\n'.format(spe.name)) # 基组（直接使用根据vasp拟合的基组以及默认的基函数）
+            # 这里会出现一个非常严重的计算问题！！！！！！！！！！！！！！
+            # lobster认为你指定的原子对是不具有周期性的
+            # 你用pymatgen脚本找到的距离是包含周期性的，把这原子对输入给lobsterin
+            # 它认不出来这个距离是周期性的，它会按照原胞内的距离考虑两个原子的成键。
+            # 所以这里我抛弃了设置原子对来计算成键强度的方法。
+            # 改用设置键长来获得原子对，lobster有自己的算法来获得原子对。
+            # for pair, idx, d in pairs_idxs_d:
+            #     if lower_d <= d <= upper_d:
+            #         f.write("cohpbetween atom {} and atom {}\n".format(idx[0], idx[1]))
+            f.write("cohpGenerator from {} to {} type {} type {}\n".format(lower_d, upper_d, species_custom1, species_custom2))
 
 
 
 dirs = "{}_{}_{}_{}".format(species_custom1, species_custom2, lower_d, upper_d)
 if not os.path.exists(dirs):
     os.mkdir(dirs)
-print("Note: ------------------------------------------------------")
-print("    You need to do it before running Lobster-4.1.0")
-print("    cp lobsterraw lobsterin")
-print("    You need to do it after running Lobster-4.1.0")
-print("    cp {COBICAR.lobster,COHPCAR.lobster,COOPCAR.lobster,ICOBILIST.lobster,ICOHPLIST.lobster,ICOOPLIST.lobster,lobsterin}" + "  {}".format(dirs))
-print("------------------------------------------------------------")
+
+import os
+
+files = ['WAVECAR', 'CONTCAR', 'KPOINTS', 'OUTCAR', 'POTCAR', 'vasprun.xml']
+
+for file in files:
+    if os.path.exists(file):
+        os.system(f"ln -s {os.path.abspath(file)} {dirs}")
+    else:
+        print(f"{file} does not exist.")
+
+write_lobsterin(dirs, mode=mode)
+
+info = """Note: -------------------------------
+1. 如果运行了lobster得不到COHP，说明NBANDS不够多
+2. 如果运行了vasp发现不收敛，可以尝试做如下改变
+    ISTART  = 0   ---->  ISTART  = 1   读取波函数
+    ICHARG  = 2   ---->  ICHARG  = 11  读取电荷密度
+"""
+print(info)
